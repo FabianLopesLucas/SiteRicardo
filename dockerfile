79c2987f3ecbd48d0c0df3355a81f47939b7d94a
Fabian <lopesfabian315@gmail.com>
2025-05-06 17:04:04 -0300

    Envio do Site

---
+++ dockerfile
@@ -0,0 +1,18 @@
+# Usa imagem oficial do PHP com Apache
+FROM php:8.2-apache
+
+# Copia os arquivos do projeto para o diretório raiz do Apache
+COPY . /var/www/html/
+
+# Dá permissões apropriadas
+RUN chown -R www-data:www-data /var/www/html \
+    && chmod -R 755 /var/www/html
+
+# Habilita o mod_rewrite
+RUN a2enmod rewrite
+
+# Permite que o Apache use .htaccess
+RUN sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+
+# Instala extensões PHP
+RUN docker-php-ext-install mysqli pdo pdo_mysql
---
+++ partials/head.php
@@ -0,0 +1,22 @@
+<head>
+<meta charset="utf-8">
+<title>Braine Digital Agency Business HTML-5 Template | Services</title>
+<!-- Stylesheets -->
+<link href="assets/css/bootstrap.css" rel="stylesheet">
+<link href="assets/css/style.css" rel="stylesheet">
+<link href="assets/css/meanmenu.min.css" rel="stylesheet">
+<link href="assets/css/responsive.css" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,600;1,700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+<link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+<link rel="icon" href="assets/images/favicon.png" type="image/x-icon">
+
+<!-- Responsive -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<script src="https://static.elfsight.com/platform/platform.js" async></script>
+<div class="elfsight-app-35988f47-a1af-493e-ba55-16927de6620a" data-elfsight-app-lazy></div>
+
+</head>
